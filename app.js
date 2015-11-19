9f252a48d81b0ee26c474827ba87b72203f38e71
Leon Thorne <ldthorne@brandeis.edu>
2015-11-19 09:37:16 -0500

    changed stuff

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+// var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var swig = require('swig');
+// require('./filters')(swig);
+var path = require('path');
+module.exports = app;
+
+//swig stuff--------------------------------------
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+
+//------------------------------------------------
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get("/", function(req,res){
+	res.render("index")
+})
+
+app.post("/", function(req,res){
+	res.render("success")
+	// ("/public/success.jpg")
+})
+
+app.listen(3000, function(){
+	console.log("listening on port 3000")
+})
